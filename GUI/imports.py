b9adb51f086b1c9806669216998fe14bf9bdcbe6
qbates18 <qbates@scu.edu>
2023-09-11 15:35:48 -0700

    Deprecated Code Cleaning: Removed Socket and controls.py
    Removed files left from when ROS and socket communication was used to send messages between topside and subsea, which was removed in 2021

--- GUI/imports.py
+++ GUI/imports.py
@@ -7,14 +7,13 @@ from tkinter import messagebox
 from config import *
 
 from packages.models.generator import generate
-from packages.models.parser import parse
+
 
 from packages.models.frames import *
 
 from packages.gamepad.interpreter import interpret, interpret2, generate_dictionaries
 from packages.gamepad.ps4 import PS4Controller as Gamepad
 
-from packages.socket.socket import socket_send #Q: I think this is obsolete
 
 from packages.camera.cam import *
 
@@ -26,7 +25,6 @@ from packages.data_logging.data_log import *
 from queue import Queue
 from threading import Thread
 
-import socket #Q: I think this is obsolete
 import serial
 
 
